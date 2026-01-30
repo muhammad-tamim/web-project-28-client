b00549e2418b1f6a8f72183c2f3bd2b47ad8cfa4
muhammad-tamim <contact2tamim@gmail.com>
2026-01-30 18:45:20 +0600

    chore(main.jsx): add react hot toast

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import { RouterProvider } from 'react-router';
 import { Router } from './routes/Router';
 import AuthProvider from './providers/AuthProvider';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <RouterProvider router={Router} />
+      <Toaster position='top-right'></Toaster>
     </AuthProvider>
   </StrictMode>
 )
