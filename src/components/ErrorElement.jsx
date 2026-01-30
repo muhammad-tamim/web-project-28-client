2d9d24eec5a5caabac64287b1da21370ee6f263f
muhammad-tamim <contact2tamim@gmail.com>
2026-01-30 22:52:55 +0600

    feat(components): add error element

--- src/components/ErrorElement.jsx
+++ src/components/ErrorElement.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link, useRouteError } from 'react-router';
 
 const ErrorElement = () => {
+    const error = useRouteError()
     return (
-        <div>
-            ErrorElement
+        <div className="flex flex-col items-center justify-center min-h-screen text-center">
+            <h1 className="text-3xl font-bold mb-2">Something went wrong 😢</h1>
+            <p className="mb-4 text-gray-600">{error?.status || "Unknown"}</p>
+            <p className="mb-4 text-gray-600">{error?.statusText || "No status text"}</p>
+            <p className="mb-4 text-gray-600">{error?.message || "No message provided"}</p>
+            {error?.data && (
+                <p className="mb-4 text-gray-600">{JSON.stringify(error.data)}</p>
+            )}
+            <Link to="/" className="text-blue-600 underline">
+                Go Home
+            </Link>
         </div>
     );
 };
