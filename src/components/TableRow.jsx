b040d0e071be23d9054ed0360741432970b041ff
muhammad-tamim <contact2tamim@gmail.com>
2026-01-31 20:20:28 +0600

    feat(components): add table row

---
+++ src/components/TableRow.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const TableRow = ({ car }) => {
+    return (
+        <tr
+            className="border border-gray-200"
+        >
+            <td className="p-2">
+                <img
+                    src={car.imageUrl}
+                    alt={car.carModel}
+                    className="w-24 h-16 object-cover rounded"
+                />
+            </td>
+            <td className="p-2 font-semibold">{car.carModel}</td>
+            <td className="p-2 truncate max-w-xs">
+                {car.description ? car.description.substring(0, 60) + '...' : 'No description'}
+            </td>
+            <td className="p-2">
+                <Link to={`/car-details/${car._id}`} className="btn btn-sm btn-primary">
+                    Book Now
+                </Link>
+            </td>
+        </tr>
+    );
+};
+
+export default TableRow;
