ed613f4a215a67a4d709aec71ebf14144bd18bbe
muhammad-tamim <contact2tamim@gmail.com>
2026-02-02 09:32:12 +0600

    refactor(components/MaxWidth.jsx): add pading for all devices

--- src/components/MaxWidth.jsx
+++ src/components/MaxWidth.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MaxWidth = ({ children }) => {
     return (
-        <div className="max-w-480 mx-auto">
+        <div className="max-w-7xl mx-auto px-5 md:px-10 xl:px-0">
             {children}
         </div>
     );
