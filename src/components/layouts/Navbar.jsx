1238df9f3844d5bafc6e90c60833472e58b34c2d
muhammad-tamim <contact2tamim@gmail.com>
2026-02-02 09:29:57 +0600

    refactor(navbar): improve the full navbar ui

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,17 +1,33 @@
-import React, { useContext } from 'react';
+import React, { use, useEffect, useState } from 'react';
+import MaxWidth from '../MaxWidth';
 import { AuthContext } from '../../contexts/AuthContext';
-import logo from '../../assets/logos/demo-logo.png'
-import { Link, NavLink } from 'react-router';
+import { NavLink } from 'react-router';
+import { MdWbSunny } from 'react-icons/md';
+import { FaMoon } from 'react-icons/fa';
+import { GiShoppingCart } from 'react-icons/gi';
+import { CiMenuFries } from 'react-icons/ci';
 import toast from 'react-hot-toast';
 
 const Navbar = () => {
+    const { user, signOutUser } = use(AuthContext)
+    const [scrolled, setScrolled] = useState(false);
 
-    const { user, signOutUser } = useContext(AuthContext)
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 20) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+    }, []);
 
     const handleSignOut = () => {
         signOutUser()
             .then(() => {
-                toast.error("Logout Successfully")
+                toast.success("Signed Out")
             })
             .then((err) => {
                 toast.error(err.message)
@@ -19,95 +35,133 @@ const Navbar = () => {
     }
 
     return (
-        <div className="px-4 py-0 lg:px-10 navbar bg-base-100 border-b border-base-300 shadow-sm sticky top-0 z-50">
-            <div className="navbar-start">
-                {/* Drop Down For Small Devices */}
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn bg-base-300 btn-sm lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
+        <div className={`w-full fixed top-0 left-0 z-50  transition-all duration-500 ease-out ${scrolled ? 'bg-base-100 shadow-md' : 'bg-transparent'}`}>
+            <MaxWidth>
+                <div className="justify-between items-center py-5 hidden lg:flex">
+
+                    <div>
+                        <h3 className='text-3xl font-bold tracking-wider'><span className='text-[#f5b754]'>R</span>ENAX</h3>
                     </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 z-1 mt-3 w-52 p-2 shadow"
-                    >
-                        <li><NavLink to="/" className={({ isActive }) => isActive ? "text-primary" : ''}>Home</NavLink></li>
-                        <li><NavLink to="/available-cars" className={({ isActive }) => isActive ? "text-primary" : ''}>Available Cars</NavLink></li>
-                        {user && (
-                            <>
-                                <li><NavLink to="/add-car" className={({ isActive }) => isActive ? "text-primary" : ''}>Add Car</NavLink></li>
-                                <li><NavLink to="/my-cars" className={({ isActive }) => isActive ? "text-primary" : ''}>My Cars</NavLink></li>
-                                <li><NavLink to="/my-bookings" className={({ isActive }) => isActive ? "text-primary" : ''}>My Bookings</NavLink></li>
-                            </>
-                        )}
-                        <li>
-                            {user ? (
-                                <NavLink to="/sign-in" className={({ isActive }) => isActive ? "text-primary" : 'hover:text-primary'}>
+
+                    <div>
+                        <ul className="flex items-center gap-5">
+                            <li><NavLink to="/" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>Home</NavLink></li>
+                            <li><NavLink to="/about" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>About</NavLink></li>
+                            <li><NavLink to="/available-cars" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>Available Cars</NavLink></li>
+                            {user &&
+                                <>
+                                    <li><NavLink to="/add-car" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>Add Car</NavLink></li>
+                                    <li><NavLink to="/my-cars" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>My Cars</NavLink></li>
+                                </>
+                            }
+                            <li><NavLink to="/blog" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>Blog</NavLink></li>
+                            <li><NavLink to="/contact" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>Contact</NavLink></li>
+                            {user ?
+                                <li><a onClick={handleSignOut} className="cursor-pointer hover:text-[#f5b754]">SignOut</a></li>
+                                :
+                                <li><NavLink to="/sign-in" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>
                                     SignIn
-                                </NavLink>
-                            ) : (
-                                <a onClick={signOutUser} className="cursor-pointer hover:text-primary">
-                                    SignOut
-                                </a>
-                            )}
-                        </li>
-                    </ul>
+                                </NavLink></li>
+                            }
+                            <li>
+                                <label className="toggle">
+                                    <input type="checkbox" value="dark" className="theme-controller" />
+                                    <MdWbSunny />
+                                    <FaMoon />
+                                </label>
+                            </li>
+                            <li>
+                                {user &&
+                                    <NavLink to="/my-bookings" className={({ isActive }) => isActive ? "text-[#f5b754] indicator flex items-center" : 'indicator flex items-center'}>
+                                        <GiShoppingCart className='size-8' />
+                                        <span className="badge bg-transparent border-none badge-xs font-bold indicator-item">8</span>
+                                    </NavLink>
+                                }
+                            </li>
+                            <li>
+                                {user && (
+                                    <div className="avatar">
+                                        <div className="w-9 rounded-full ">
+                                            <img alt="" src={user?.photoURL} />
+                                        </div>
+                                    </div>
+                                )}
+                            </li>
+                        </ul>
+                    </div>
+
                 </div>
-                {/* Logo */}
-                <Link to="/" className='cursor-pointer ml-2 lg:ml-0 flex items-center gap-1 size-20'>
-                    <img src={logo} alt="logo.png" />
-                </Link>
-            </div>
 
-            {/* Navbar center links */}
-            <div className="navbar-center hidden lg:flex xl:text-xl">
-                <ul className="flex gap-5 px-1">
-                    <li><NavLink to="/" className={({ isActive }) => isActive ? "text-primary" : ''}>Home</NavLink></li>
-                    <li><NavLink to="/available-cars" className={({ isActive }) => isActive ? "text-primary" : ''}>Available Cars</NavLink></li>
-                    {user && (
-                        <>
-                            <li><NavLink to="/add-car" className={({ isActive }) => isActive ? "text-primary" : ''}>Add Car</NavLink></li>
-                            <li><NavLink to="/my-cars" className={({ isActive }) => isActive ? "text-primary" : ''}>My Cars</NavLink></li>
-                            <li><NavLink to="/my-bookings" className={({ isActive }) => isActive ? "text-primary" : ''}>My Bookings</NavLink></li>
-                        </>
-                    )}
-                    <li>
-                        {!user ? (
-                            <NavLink to="/sign-in" className={({ isActive }) => isActive ? "text-primary" : 'hover:text-primary'}>
-                                SignIn
-                            </NavLink>
-                        ) : (
-                            <a onClick={handleSignOut} className="cursor-pointer hover:text-primary">
-                                SignOut
-                            </a>
-                        )}
-                    </li>
-                </ul>
-            </div>
+                {/* for small devices */}
+                <div className='flex justify-between items-center py-5 lg:hidden'>
+                    <div>
+                        <h3 className='text-xl md:text-3xl font-bold tracking-wider'><span className='text-[#f5b754]'>R</span>ENAX</h3>
+                    </div>
 
-            {/* Navbar end */}
-            <div className="navbar-end gap-5 xl:text-xl">
-                {/* Theme toggle button */}
-                <label className="toggle text-base-content">
-                    <input type="checkbox" value="dark" className="theme-controller" />
+                    <div className='flex items-center gap-2 md:gap-5'>
+                        <ul className="flex items-center gap-5">
+                            <li>
+                                <label className="toggle">
+                                    <input type="checkbox" value="dark" className="theme-controller" />
+                                    <MdWbSunny />
+                                    <FaMoon />
+                                </label>
+                            </li>
+                            <li>
+                                {user &&
+                                    <NavLink to="/my-bookings" className={({ isActive }) => isActive ? "text-[#f5b754] indicator flex items-center" : 'indicator flex items-center'}>
+                                        <GiShoppingCart className='size-8' />
+                                        <span className="badge bg-transparent border-none badge-xs font-bold indicator-item">8</span>
+                                    </NavLink>
+                                }
+                            </li>
+                            <li>
+                                {user && (
+                                    <div className="avatar">
+                                        <div className="w-9 rounded-full ">
+                                            <img alt="" src={user?.photoURL} />
+                                        </div>
+                                    </div>
+                                )}
+                            </li>
+                        </ul>
 
-                    <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
-
-                    <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></g></svg>
-
-                </label>
-
-                {user && (
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img alt="" src={user?.photoURL} />
+                        <div className="drawer drawer-end">
+                            <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
+                            <div className="drawer-content">
+                                <label htmlFor="my-drawer-1" className="cursor-pointer drawer-button">
+                                    <CiMenuFries size={30} />
+                                </label>
+                            </div>
+                            <div className="drawer-side">
+                                <label htmlFor="my-drawer-1" aria-label="close sidebar" className="drawer-overlay"></label>
+                                <ul className="menu bg-base-200 min-h-full w-40 md:w-80 p-4">
+                                    <li><NavLink to="/" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>Home</NavLink></li>
+                                    <li><NavLink to="/about" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>About</NavLink></li>
+                                    <li><NavLink to="/available-cars" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>Available Cars</NavLink></li>
+                                    {user &&
+                                        <>
+                                            <li><NavLink to="/add-car" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>Add Car</NavLink></li>
+                                            <li><NavLink to="/my-cars" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>My Cars</NavLink></li>
+                                        </>
+                                    }
+                                    <li><NavLink to="/blog" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>Blog</NavLink></li>
+                                    <li><NavLink to="/contact" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>Contact</NavLink></li>
+                                    {user ?
+                                        <li><a onClick={handleSignOut} className="cursor-pointer hover:text-[#f5b754]">SignOut</a></li>
+                                        :
+                                        <li><NavLink to="/sign-in" className={({ isActive }) => isActive ? "text-[#f5b754]" : ''}>
+                                            SignIn
+                                        </NavLink></li>
+                                    }
+                                </ul>
                             </div>
                         </div>
                     </div>
-                )}
-            </div>
+
+                </div>
+
+            </MaxWidth>
         </div>
     );
 };
