67f7624eeea588e75d37acd8aa773a4b25828ce4
muhammad-tamim <contact2tamim@gmail.com>
2026-02-04 20:36:40 +0600

    refactor(components/LoadingSpinner): make loading spinner color to primary color

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LoadingSpinner = ({ minHScreen, marginY }) => {
     return (
         <div className={`${minHScreen} ${marginY} flex justify-center items-center`}>
-            <span className="loading loading-spinner size-20"></span>
+            <span className="loading loading-spinner size-20 text-primary"></span>
         </div>
     );
 };
