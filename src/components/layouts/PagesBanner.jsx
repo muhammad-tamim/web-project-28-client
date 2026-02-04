18cda9114d08f955401e8db0ec5cc949a4d08c39
muhammad-tamim <contact2tamim@gmail.com>
2026-02-04 20:27:14 +0600

    refactror(Components/layouts): make PageBanner pageName props conditional rendaring

--- src/components/layouts/PagesBanner.jsx
+++ src/components/layouts/PagesBanner.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 
-const PagesBanner = ({ title, pageName, bg }) => {
+const PagesBanner = ({ title, pageName = '', bg }) => {
     return (
         <div className='relative min-h-[70vh] overflow-hidden' >
             <img src={bg} className='absolute inset-0 w-full h-full object-cover' alt="" />
             <div className="absolute inset-0 bg-black/60"></div>
 
             <div className='absolute inset-0  flex flex-col justify-center items-center gap-3'>
-                <p className='text-primary text-[11px] tracking-[5px]'>{pageName.toUpperCase()}</p>
+                {pageName && (
+                    <p className="text-primary text-[11px] tracking-[5px]">
+                        {pageName.toUpperCase()}
+                    </p>
+                )}
                 <h1 className='font-bold text-3xl md:text-4xl text-white text-center'>{title.toUpperCase()}</h1>
             </div>
 
