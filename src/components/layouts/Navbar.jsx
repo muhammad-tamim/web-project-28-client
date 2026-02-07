5067f0039e81e88d8de79713da518cc568d6e02d
muhammad-tamim <contact2tamim@gmail.com>
2026-02-07 20:26:45 +0600

    refactor(Navbar): make profile image to dashboard dropdown

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -112,10 +112,17 @@ const Navbar = () => {
                             </li>
                             <li>
                                 {user && (
-                                    <div className="avatar">
-                                        <div className="w-9 rounded-full ">
-                                            <img alt="" src={user?.photoURL} />
+                                    <div className="dropdown dropdown-end">
+                                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                            <div className="w-10 rounded-full">
+                                                <img alt="" src={user?.photoURL} />
+                                            </div>
                                         </div>
+                                        <ul
+                                            tabIndex="-1"
+                                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                            <li><NavLink to={'/dashboard'} className={({ isActive }) => isActive ? "text-[#f5b754]" : 'text-secondary'}>Dashboard</NavLink></li>
+                                        </ul>
                                     </div>
                                 )}
                             </li>
@@ -159,10 +166,17 @@ const Navbar = () => {
                             </li>
                             <li>
                                 {user && (
-                                    <div className="avatar">
-                                        <div className="w-9 rounded-full ">
-                                            <img alt="" src={user?.photoURL} />
+                                    <div className="dropdown dropdown-end">
+                                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                            <div className="w-10 rounded-full">
+                                                <img alt="" src={user?.photoURL} />
+                                            </div>
                                         </div>
+                                        <ul
+                                            tabIndex="-1"
+                                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                            <li><NavLink className={({ isActive }) => isActive ? "text-[#f5b754]" : 'text-secondary'} to={'/dashboard'}>Dashboard</NavLink></li>
+                                        </ul>
                                     </div>
                                 )}
                             </li>
@@ -171,11 +185,11 @@ const Navbar = () => {
                         <div className="drawer drawer-end">
                             <input id="drawer-medium" type="checkbox" className="drawer-toggle" />
                             <div className="drawer-content">
-                                <label htmlFor="drawer-md" className="cursor-pointer drawer-button text-gray-500 hover:text-primary">
+                                <label htmlFor="drawer-medium" className="cursor-pointer drawer-button text-gray-500 hover:text-primary">
                                     <CiMenuFries size={30} />
                                 </label>
                             </div>
-                            <div className="drawer-medium">
+                            <div className="drawer-side">
                                 <label htmlFor="drawer-medium" aria-label="close sidebar" className="drawer-overlay"></label>
                                 <ul className="menu bg-base-200 min-h-full w-48 md:w-80 p-4">
                                     <li><NavLink to="/" className={({ isActive }) => isActive ? "text-[#f5b754]" : 'text-secondary'}>Home</NavLink></li>
@@ -250,10 +264,17 @@ const Navbar = () => {
                             </li>
                             <li>
                                 {user && (
-                                    <div className="avatar">
-                                        <div className="w-9 rounded-full ">
-                                            <img alt="" src={user?.photoURL} />
+                                    <div className="dropdown dropdown-end">
+                                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                            <div className="w-10 rounded-full">
+                                                <img alt="" src={user?.photoURL} />
+                                            </div>
                                         </div>
+                                        <ul
+                                            tabIndex="-1"
+                                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                            <li><NavLink className={({ isActive }) => isActive ? "text-[#f5b754]" : 'text-secondary'} to={'/dashboard'}>Dashboard</NavLink></li>
+                                        </ul>
                                     </div>
                                 )}
                             </li>
@@ -316,8 +337,8 @@ const Navbar = () => {
 
                 </div>
 
-            </MaxWidth>
-        </div>
+            </MaxWidth >
+        </div >
     );
 };
 
