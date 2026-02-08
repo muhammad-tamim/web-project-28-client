15de1b044c148e4a00f2c687df6138f3abaa7e57
muhammad-tamim <contact2tamim@gmail.com>
2026-02-08 16:24:52 +0600

    refactor(components/home/rentalProcess): add cards 2

--- src/components/Home/RentalProcess.jsx
+++ src/components/Home/RentalProcess.jsx
@@ -3,6 +3,30 @@ import { FaInfoCircle } from 'react-icons/fa';
 import { MdArrowOutward } from 'react-icons/md';
 import { Link } from 'react-router';
 
+const rentalProcessArray = [
+    {
+        number: 1,
+        title: 'Choose A Car',
+        paragraph: 'Browse our wide selection of vehicles and compare features, prices, and availability to find the perfect car for your trip.',
+        routes: '/available-cars',
+        btnName: 'Available Cars'
+    },
+    {
+        number: 2,
+        title: 'Book A Car',
+        paragraph: 'Select your preferred dates, provide the required details, and confirm your booking in just a few simple steps.',
+        routes: '/available-cars',
+        btnName: 'Available Cars'
+    },
+    {
+        number: 3,
+        title: 'Verify Bookings',
+        paragraph: 'Review your booking status, check rental details, and stay updated on approvals or changes from the owner.',
+        routes: '/my-bookings',
+        btnName: 'My Bookings'
+    },
+]
+
 const RentalProcess = () => {
     return (
         <div>
@@ -19,32 +43,15 @@ const RentalProcess = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
 
 
-                <div className="card rounded-3xl p-10 bg-base-300 shadow-sm space-y-8">
+                {rentalProcessArray.map(process => <div key={process.number} className="card rounded-3xl p-10 bg-base-300 shadow-sm space-y-8">
                     <div className='flex items-center gap-5'>
-                        <button className="right-1 btn btn-lg btn-circle btn-primary">1</button>
-                        <h1 className='text-2xl font-medium'>Choose A Car</h1>
+                        <button className="right-1 btn btn-lg btn-circle btn-primary">{process.number}</button>
+                        <h1 className='text-2xl font-medium'>{process.title}</h1>
                     </div>
-                    <p className='text-gray-500 text-sm'>Browse our wide selection of vehicles and compare features, prices, and availability to find the perfect car for your trip.</p>
-                    <Link to={'/available-cars'}><button className='btn btn-primary btn-outline rounded-full btn-xl hover:-translate-y-1 duration-200 transition flex items-center gap-1'>Available Cars <span><MdArrowOutward /></span></button></Link>
-                </div>
+                    <p className='text-gray-500 text-sm'>{process.paragraph}</p>
+                    <Link to={`${process.routes}`}><button className='btn btn-primary btn-outline rounded-full btn-xl hover:-translate-y-1 duration-200 transition flex items-center gap-1'>{process.btnName} <span><MdArrowOutward /></span></button></Link>
+                </div>)}
 
-                <div className="card rounded-3xl p-10 bg-base-300 shadow-sm space-y-8">
-                    <div className='flex items-center gap-5'>
-                        <button className="right-1 btn btn-lg btn-circle btn-primary">2</button>
-                        <h1 className='text-2xl font-medium'>Book A Car</h1>
-                    </div>
-                    <p className='text-gray-500 text-sm'>Select your preferred dates, provide the required details, and confirm your booking in just a few simple steps.</p>
-                    <Link to={'/available-cars'}><button className='btn btn-primary btn-outline rounded-full btn-xl hover:-translate-y-1 duration-200 transition flex items-center gap-1'>Available Cars <span><MdArrowOutward /></span></button></Link>
-                </div>
-
-                <div className="card rounded-3xl p-10 bg-base-300 shadow-sm space-y-8">
-                    <div className='flex items-center gap-5'>
-                        <button className="right-1 btn btn-lg btn-circle btn-primary">3</button>
-                        <h1 className='text-2xl font-medium'>Verify Bookings</h1>
-                    </div>
-                    <p className='text-gray-500 text-sm'>Review your booking status, check rental details, and stay updated on approvals or changes from the owner.</p>
-                    <Link to={'/my-bookings'}><button className='btn btn-primary btn-outline rounded-full btn-xl hover:-translate-y-1 duration-200 transition flex items-center gap-1'>My Bookings <span><MdArrowOutward /></span></button></Link>
-                </div>
             </div>
 
             <div className='flex gap-2 mt-10 justify-center items-center'>
