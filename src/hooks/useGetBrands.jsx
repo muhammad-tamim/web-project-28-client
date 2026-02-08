537feedddeae456c2b3bed3a6285035f3f9cfdc5
muhammad-tamim <contact2tamim@gmail.com>
2026-02-08 08:17:22 +0600

    feat(hooks): add useGetBrands hook

---
+++ src/hooks/useGetBrands.jsx
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import { brandsApi } from '../api/brands.api';
+
+const useGetBrands = () => {
+    return useQuery({
+        queryKey: ['brands'],
+        queryFn: brandsApi.getAll,
+    })
+};
+
+export default useGetBrands;
