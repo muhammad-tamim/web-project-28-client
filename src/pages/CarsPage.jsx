292b8d110b595cf3fc5f57ff6d5bd86cc15fb315
muhammad-tamim <contact2tamim@gmail.com>
2026-02-09 09:06:42 +0600

    feat(pages/carsPage): implement backeind api

--- src/pages/CarsPage.jsx
+++ src/pages/CarsPage.jsx
@@ -1,98 +1,110 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import LoadingSpinner from '../components/LoadingSpinner';
-import Card from '../components/Card';
 import TableRow from '../components/TableRow';
 import Pagination from '../components/Pagination';
+import useGetCars from '../hooks/queries/cars/useGetCars';
+import PagesBanner from '../components/layouts/PagesBanner';
+import MaxWidth from '../components/MaxWidth';
+import Card3 from '../components/Cards/Card3';
 
 const CarsPage = () => {
-    const [cars, setCars] = useState([]);
-    const [loading, setLoading] = useState(true);
     const [search, setSearch] = useState('');
     const [sortBy, setSortBy] = useState('newest');
     const [isGridView, setIsGridView] = useState(true);
 
-    useEffect(() => {
-        fetch(`${import.meta.env.VITE_API_URL}/cars`)
-            .then((res) => res.json())
-            .then((data) => {
-                setCars(data);
-                setLoading(false);
-            });
-    }, []);
 
-    if (loading) return <LoadingSpinner minHScreen={'min-h-screen'}></LoadingSpinner>
+    const { data: cars, isLoading, error, isError } = useGetCars();
+
+    if (isLoading) {
+        return <LoadingSpinner marginY={'20'}></LoadingSpinner>;
+    }
+
+    if (isError) {
+        return <h2 className="text-red-500 text-center my-20">Error: {error.message}</h2>
+    }
 
     // searching
-    const filteredCars = cars.filter((car) => car.carModel.toLowerCase().includes(search.toLowerCase()))
+    const filteredCars = cars.filter((car) => car.model.toLowerCase().includes(search.toLowerCase()))
 
     // sorting
     const sortedCars = [...filteredCars].sort((a, b) => {
         if (sortBy === 'newest') {
-            return new Date(b.postedDate) - new Date(a.postedDate);
-        } else if (sortBy === 'oldest') {
-            return new Date(a.postedDate) - new Date(b.postedDate);
-        } else if (sortBy === 'lowest') {
-            return parseFloat(a.rentalPrice) - parseFloat(b.rentalPrice);
-        } else if (sortBy === 'highest') {
-            return parseFloat(b.rentalPrice) - parseFloat(a.rentalPrice);
+            return new Date(b.createdAt) - new Date(a.createdAt)
         }
-    });
+        if (sortBy === 'oldest') {
+            return new Date(a.createdAt) - new Date(b.createdAt)
+        }
+        if (sortBy === 'lowest') {
+            return a.dailyRentalPrice - b.dailyRentalPrice
+        }
+        if (sortBy === 'highest') {
+            return b.dailyRentalPrice - a.dailyRentalPrice
+        }
+        return 0
+    })
+
 
     return (
-        <div className='px-4 lg:px-10 my-20 space-y-20'>
-            <h1 className='text-center text-4xl font-bold mb-10'>Recent Listing</h1>
+        <div>
 
-            <div className='flex flex-col lg:flex-row justify-between items-center gap-4 mb-8'>
-                <input type='text' placeholder='Search...' className='input input-bordered w-full lg:max-w-sm border-gray-200 '
-                    onChange={(e) => setSearch(e.target.value)} />
+            <PagesBanner pageName={'rentax'} title={'Available Cars'}></PagesBanner>
 
-                <div className='flex gap-4'>
-                    <select className='select select-bordered border-gray-200'
-                        onChange={(e) => setSortBy(e.target.value)} >
-                        <option value='newest'>Newest First</option>
-                        <option value='oldest'>Oldest First</option>
-                        <option value='lowest'>Price: Low to High</option>
-                        <option value='highest'>Price: High to Low</option>
-                    </select>
+            <MaxWidth>
+                <div className='space-y-20 my-20'>
+                    <div className='flex flex-col lg:flex-row justify-between items-center gap-4 mb-8'>
+                        <input type='text' placeholder='Search...' className='input input-bordered w-full lg:max-w-sm border-gray-200 '
+                            onChange={(e) => setSearch(e.target.value)} />
 
-                    <button
-                        onClick={() => setIsGridView(!isGridView)}
-                        className='btn btn-outline btn-primary'
-                    >
-                        {isGridView ? 'List View' : 'Grid View'}
-                    </button>
+                        <div className='flex gap-4'>
+                            <select className='select select-bordered border-gray-200'
+                                onChange={(e) => setSortBy(e.target.value)} >
+                                <option value='newest'>Newest First</option>
+                                <option value='oldest'>Oldest First</option>
+                                <option value='lowest'>Price: Low to High</option>
+                                <option value='highest'>Price: High to Low</option>
+                            </select>
+
+                            <button
+                                onClick={() => setIsGridView(!isGridView)}
+                                className='btn btn-outline btn-primary'
+                            >
+                                {isGridView ? 'List View' : 'Grid View'}
+                            </button>
+                        </div>
+                    </div>
+
+
+
+                    {isGridView ? (
+                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                            {sortedCars.map((car) => <Card3 car={car} key={car._id}></Card3>)}
+                        </div>
+                    ) : (
+                        <div className="overflow-x-auto w-full rounded-md border border-gray-200">
+                            <table className="table-auto w-full ">
+                                <thead className="">
+                                    <tr>
+                                        <th className="p-3 text-left">Image</th>
+                                        <th className="p-3 text-left">Model</th>
+                                        <th className="p-3 text-left">Description</th>
+                                        <th className="p-3 text-left">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {sortedCars.map((car) => (
+                                        <TableRow key={car._id} car={car} />
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    )}
+
+                    <div className='flex justify-center'>
+                        <Pagination></Pagination>
+                    </div>
                 </div>
-            </div>
+            </MaxWidth>
 
-
-
-            {isGridView ? (
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
-                    {sortedCars.map((car) => <Card car={car} key={car._id}></Card>)}
-                </div>
-            ) : (
-                <div className="overflow-x-auto w-full rounded-md border border-gray-200">
-                    <table className="table-auto w-full ">
-                        <thead className="">
-                            <tr>
-                                <th className="p-3 text-left">Image</th>
-                                <th className="p-3 text-left">Model</th>
-                                <th className="p-3 text-left">Description</th>
-                                <th className="p-3 text-left">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {sortedCars.map((car) => (
-                                <TableRow key={car._id} car={car} />
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            )}
-
-            <div className='flex justify-center'>
-                <Pagination></Pagination>
-            </div>
         </div>
     );
 };
