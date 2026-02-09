49f4791331ce1c2d4c78cda798c6b1c482c4abca
muhammad-tamim <contact2tamim@gmail.com>
2026-02-09 06:50:44 +0600

    refactor(components/loadingSpinner) remove size props

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const LoadingSpinner = ({ minHScreen, marginY, size = 20 }) => {
+const LoadingSpinner = ({ minHScreen, marginY }) => {
     return (
         <div className={`${minHScreen} ${marginY} flex justify-center items-center`}>
-            <span className={`loading loading-spinner ${size} text-primary`}></span>
+            <span className={`loading loading-spinner size-20 text-primary`}></span>
         </div>
     );
 };
