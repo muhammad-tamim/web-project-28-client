0f8571740ea514d7b276162d91e1b3365e7ab9ed
muhammad-tamim <contact2tamim@gmail.com>
2026-02-09 11:59:49 +0600

    feat(pagination): implement backend logic

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 
-const Pagination = () => {
+const Pagination = ({ page, totalPages, onPageChange }) => {
+
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i);
+    }
+
+    const handlePrevious = () => {
+        if (page > 1) onPageChange(page - 1);
+    };
+
+    const handleNext = () => {
+        if (page < totalPages) onPageChange(page + 1);
+    };
+
+
     return (
         <ul className='space-x-2'>
-            <li className='btn btn-circle btn-lg btn-primary text-2xl'>
+            <li className={`btn btn-circle btn-lg btn-primary text-2xl ${page === 1 ? 'btn-disabled opacity-50 cursor-not-allowed' : ''}`}
+                onClick={handlePrevious}>
                 <MdKeyboardArrowLeft />
             </li>
-            <li className='btn btn-circle btn-lg btn-primary text-2xl'>1</li>
-            <li className='btn btn-circle btn-lg btn-primary text-2xl'>2</li>
-            <li className='btn btn-circle btn-lg btn-primary text-2xl'>3</li>
-            <li className='btn btn-circle btn-lg btn-primary text-2xl'>
+            {pageNumbers.map(num => <li className={`btn btn-circle btn-lg text-2xl ${num === page ? 'btn-primary' : 'btn-outline btn-primary'}`}
+                onClick={() => onPageChange(num)}>{num}</li>)}
+            <li className={`btn btn-circle btn-lg btn-primary text-2xl ${page === totalPages ? 'btn-disabled opacity-50 cursor-not-allowed' : ''}`}
+                onClick={handleNext}>
                 <MdKeyboardArrowRight />
             </li>
         </ul>
