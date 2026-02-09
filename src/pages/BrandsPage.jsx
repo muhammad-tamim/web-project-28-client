7531c62db7f02072c38fe0b8d7f71dc68d7d6726
muhammad-tamim <contact2tamim@gmail.com>
2026-02-09 21:46:57 +0600

    feat(pages/brandsPage): fetch data by backend

--- src/pages/BrandsPage.jsx
+++ src/pages/BrandsPage.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import PagesBanner from '../components/layouts/PagesBanner';
+import MaxWidth from '../components/MaxWidth';
+import useGetBrands from '../hooks/queries/Brands/useGetBrands';
+import LoadingSpinner from '../components/LoadingSpinner';
+import Card1 from '../components/Cards/Card1';
 
 const BrandsPage = () => {
+    const { data: brands, isLoading, error, isError } = useGetBrands();
+
+    if (isLoading) {
+        return <LoadingSpinner minHScreen={'min-h-screen'}></LoadingSpinner>;
+    }
+
+    if (isError) {
+        return <h2 className="text-red-500 text-center my-20">Error: {error.message}</h2>
+    }
     return (
         <div>
-            Brands Page
+            <PagesBanner pageName={'rentax'} title={'All Brands'}></PagesBanner>
+            <MaxWidth>
+                <div className='space-y-20 my-20'>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                        {brands.map(brand => <Card1 key={brand._id} image={brand.photoUrl} title={brand.name} where={`/categories/brands-details/${brand.name}`} imageClass='m-20' hideOverlay bg></Card1>
+                        )}
+                    </div>
+                </div>
+            </MaxWidth>
         </div>
     );
 };
