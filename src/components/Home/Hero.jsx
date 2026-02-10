e5c1493861c5b495db83e59c63bd27eb1994ebef
muhammad-tamim <contact2tamim@gmail.com>
2026-02-10 22:40:28 +0600

    feat(components/home/hero): add search logic

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router';
 import heroVideo from '../../assets/videos/hero-video.mp4'
+import useGetBrands from '../../hooks/queries/Brands/useGetBrands';
+import useGetCategories from '../../hooks/queries/Categories/useGetCategories';
 
 const Hero = () => {
+    const [brand, setBrand] = useState('')
+    const [category, setCategory] = useState('')
+    const [sort, setSort] = useState('')
+
+    const { data: brands } = useGetBrands();
+    const { data: categories } = useGetCategories();
+
+    console.log(brand, category, sort)
+
     return (
         <div className='hero min-h-screen relative overflow-hidden'>
 
@@ -24,34 +35,30 @@ const Hero = () => {
 
                     <div className='grid grid-cols-1 md:grid-cols-4 gap-4 w-full'>
 
-                        <select defaultValue="Pick a color" className="select focus:outline-none outline-none border-none text-gray-500 w-full">
-                            <option disabled={true}>Any Brand</option>
-                            <option>Crimson</option>
-                            <option>Amber</option>
-                            <option>Velvet</option>
+                        <select value={brand} onChange={(e) => setBrand(e.target.value)} className="select focus:outline-none outline-none border-none text-gray-500 w-full">
+                            <option value={''}>Any Brand</option>
+                            {brands?.map(brand => <option key={brand._id}>{brand.name}</option>)}
                         </select>
 
-                        <select defaultValue="Pick a color" className="select focus:outline-none outline-none border-none text-gray-500 w-full">
-                            <option disabled={true}>Any Type</option>
-                            <option>Crimson</option>
-                            <option>Amber</option>
-                            <option>Velvet</option>
+                        <select value={category} onChange={(e) => setCategory(e.target.value)} className="select focus:outline-none outline-none border-none text-gray-500 w-full">
+                            <option value={''}>Any Category</option>
+                            {categories?.map(category => <option key={category._id}>{category.name}</option>)}
                         </select>
 
-                        <select defaultValue="Pick a color" className="select focus:outline-none outline-none border-none text-gray-500 w-full">
-                            <option disabled={true}>Default Price</option>
-                            <option>Price Low to High</option>
-                            <option>Price High to Low</option>
+                        <select value={sort} onChange={(e) => setSort(e.target.value)} className="select focus:outline-none outline-none border-none text-gray-500 w-full">
+                            <option value="">Default Price</option>
+                            <option value="asc">Price Low to High</option>
+                            <option value="desc">Price High to Low</option>
                         </select>
 
-                        <Link to="/available-cars">
+                        <Link to={`/cars/search?brand=${brand}&category=${category}&sort=${sort}`}>
                             <button className="btn btn-primary w-full text-black">Search</button>
                         </Link>
 
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
