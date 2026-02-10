14c4864083da5876caea2d5c11c5153e9ea72203
muhammad-tamim <contact2tamim@gmail.com>
2026-02-10 18:28:20 +0600

    feat(api): add bookings api

---
+++ src/api/bookings.api.js
@@ -0,0 +1,23 @@
+import { baseApi } from "./axios";
+
+export const bookingsApi = {
+    findAll: async (email) => {
+        const res = await baseApi.get(`/bookings/${email}`);
+        return res.data.result;
+    },
+
+    create: async (data) => {
+        const res = await baseApi.post("/bookings", data);
+        return res.data.result;
+    },
+
+    update: async (id, data) => {
+        const res = await baseApi.patch(`/bookings/${id}`, data);
+        return res.data.result;
+    },
+
+    delete: async (id) => {
+        const res = await baseApi.delete(`/bookings/${id}`);
+        return res.data.result;
+    },
+};
