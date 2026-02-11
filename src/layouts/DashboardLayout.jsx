9ddbc4053ddacd0c3353d03b004ec24a9701feee
muhammad-tamim <contact2tamim@gmail.com>
2026-02-11 14:13:39 +0600

    feat(layouts): add dashboard layout

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { NavLink, Outlet } from 'react-router';
+import DashboardNavbar from '../components/dashboard/Layouts/DashboardNavbar';
+import { FiFileText, FiHome } from 'react-icons/fi';
+import Sidebar from '../components/dashboard/Layouts/Sidebar';
 
 const DashboardLayout = () => {
     return (
-        <div>
+        <div className="drawer xl:drawer-open">
+
+            <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+
+            <div className="drawer-content">
+
+                <DashboardNavbar></DashboardNavbar>
+
+                <div>
+                    <Outlet></Outlet>
+                </div>
+
+            </div>
+
+            <Sidebar></Sidebar>
 
         </div>
     );
