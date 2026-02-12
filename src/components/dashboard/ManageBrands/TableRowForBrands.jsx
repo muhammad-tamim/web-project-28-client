917d613fcee4496d3165d1f2bd7578e7eda6b4b6
muhammad-tamim <contact2tamim@gmail.com>
2026-02-12 18:42:18 +0600

    feat(components/dashboard/manageBrands): add table row for brands

---
+++ src/components/dashboard/ManageBrands/TableRowForBrands.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import toast from 'react-hot-toast';
+import { Link } from 'react-router';
+import useUpdateBrand from '../../../hooks/queries/Brands/useUpdateBrand';
+import useDeleteBrand from '../../../hooks/queries/Brands/useDeleteBrand';
+import Swal from 'sweetalert2';
+
+const TableRowForBrands = ({ brand }) => {
+    const { mutate: deleteBooking } = useDeleteBrand();
+    const { mutate: updateBrand } = useUpdateBrand();
+
+    const handleDelete = () => {
+
+        Swal.fire({
+            title: "Are you sure?",
+            icon: "warning",
+            showCancelButton: true,
+
+            buttonsStyling: false,
+
+            confirmButtonText: "Yes, Delete it",
+            cancelButtonText: "Cancel",
+
+            customClass: {
+                confirmButton: "btn btn-primary mx-5 btn-lg rounded-full px-10",
+                cancelButton: "btn btn-outline btn-primary btn-lg rounded-full px-10",
+            },
+        }).then((result) => {
+            if (result.isConfirmed) {
+                deleteBooking(brand._id, {
+                    onSuccess: () => toast.success("Brand deleted successfully"),
+                    onError: (err) => toast.error(err.message || "Delete failed"),
+                });
+
+            }
+        });
+    };
+
+    const handleFormSubmit = (e) => {
+
+        e.preventDefault()
+        const photoUrl = e.target.photoUrl.value;
+        const isActive = e.target.isActive.value === "true";
+
+
+        const payload = {
+            photoUrl,
+            isActive
+        }
+
+        updateBrand({ id: brand?._id, data: payload }, {
+            onSuccess: () => {
+                toast.success("Car updated successfully");
+                document.getElementById(brand._id).close();
+            },
+            onError: (err) => toast.error(err.message || "Update failed"),
+        })
+
+    }
+
+    return (
+        <>
+            <tr className="text-secondary">
+                <td className="p-3">
+                    <div className="w-24 h-16 overflow-hidden rounded-lg">
+                        <img
+                            src={brand.photoUrl}
+                            alt={brand.name}
+                            className="w-full h-full object-cover"
+                        />
+                    </div>
+                </td>
+
+                <td className='p-3'>{brand.name}</td>
+                <td className='p-3'>{brand.carCount}</td>
+                <td className='p-3'>{brand.isActive ? 'Active' : 'Not Active'}</td>
+                <td className='p-3'>
+                    <div className="flex gap-2">
+
+                        <button onClick={() => document.getElementById(brand._id).showModal()} className={`btn btn-sm btn-primary`}>Update</button>
+                        <button onClick={handleDelete} className={`btn btn-sm btn-error ${Number(brand.carCount) > 0 && 'btn-disabled'}`}>Delete</button>
+                    </div>
+                </td>
+
+                <td>
+                    <dialog id={brand._id} className="modal">
+                        <div className="modal-box bg-base-300 rounded-3xl space-y-5">
+                            <h3 className="font-bold text-2xl text-center">
+                                Update Profile
+                            </h3>
+
+                            <form onSubmit={handleFormSubmit} className="space-y-5">
+
+
+                                <div className='space-y-2 text-secondary'>
+                                    <label className="text-sm block text-secondary font-medium">Photo URL</label>
+                                    <input name='photoUrl' type="url" defaultValue={brand.photoUrl} className='input w-full input-primary focus:outline-none bg-base-300' />
+                                </div>
+
+                                <div className='space-y-2 text-secondary'>
+                                    <label className="text-sm block text-secondary font-medium">Status</label>
+                                    <select name='isActive' defaultValue={brand.isActive ? "true" : "false"} className="select w-full select-primary focus:outline-none bg-base-300">
+                                        <option disabled={true}>Pick a Status</option>
+                                        <option value="true">Active</option>
+                                        <option value="false">In-Active</option>
+                                    </select>
+                                </div>
+
+                                <div className="modal-action flex justify-center gap-3">
+                                    <button type="submit" className='btn btn-primary rounded-full btn-xl hover:-translate-y-1 duration-200 transition'>
+                                        Update
+                                    </button>
+                                    <button type="button" onClick={() => document.getElementById(brand._id).close()} className='btn btn-primary btn-outline rounded-full btn-xl hover:-translate-y-1 duration-200 transition'>
+                                        Cancel
+                                    </button>
+
+                                </div>
+                            </form>
+                        </div>
+                    </dialog>
+                </td>
+            </tr >
+        </>
+    );
+};
+
+export default TableRowForBrands;
