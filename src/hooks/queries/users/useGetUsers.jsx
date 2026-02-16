26e914b7bfb2f27e29b14a8d908084df2cfbd725
muhammad-tamim <contact2tamim@gmail.com>
2026-02-16 14:55:37 +0600

    feat(hooks/queries/users): add get users hook

---
+++ src/hooks/queries/users/useGetUsers.jsx
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import { usersApi } from '../../../api/users.api';
+
+const useGetUsers = (page, limit = 9) => {
+    return useQuery({
+        queryKey: ['cars', page, limit],
+        queryFn: () => usersApi.getUsers(page, limit),
+        keepPreviousData: true
+    })
+};
+
+export default useGetUsers;
