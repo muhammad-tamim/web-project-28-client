fa7bfd058b77932da040fc737330c122142b1a33
muhammad-tamim <contact2tamim@gmail.com>
2026-02-17 15:00:36 +0600

    feat(dashboard): add customer statistics component for dashboard home page

--- src/components/dashboard/DashboardHomePage/Customer/CustomerStatistics.jsx
+++ src/components/dashboard/DashboardHomePage/Customer/CustomerStatistics.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
-import BookingsHistory from '../../../../pages/Dashboard/BookingsHistory';
+import { FaShoppingBag, FaMoneyBillWave } from 'react-icons/fa';
+import Card from '../Card';
+import useAuth from '../../../../hooks/useAuth';
+import useGetBookings from '../../../../hooks/queries/bookings/useGetBookings';
+import LoadingSpinner from '../../../LoadingSpinner';
+import PaymentHistory from './PaymentHistory';
+import BookingHistory from './BookingHistory';
 
 const CustomerStatistics = () => {
+
+    const { user } = useAuth();
+    const { data: bookings, isLoading, error, isError } = useGetBookings(user.email);
+
+    if (isLoading) {
+        return <LoadingSpinner minHScreen={'min-h-screen'} />;
+    }
+
+    if (isError) {
+        return (
+            <h2 className="text-red-500 text-center my-20">
+                Error: {error.message}
+            </h2>
+        );
+    }
+
+    const totalBookings = bookings.length;
+    const totalAmount = bookings.reduce((sum, booking) => sum + booking.payment.amount, 0);
+    const bestPayment = bookings.reduce((max, booking) => booking.payment.amount > max ? booking.payment.amount : max, 0)
+
     return (
-        <div>
-            Customer statistics
+        <div className='space-y-10'>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 bg-base-300 rounded-3xl p-5 md:p-10">
+                <Card title="Total Bookings" value={totalBookings} icon={<FaShoppingBag className="size-6" />} />
+                <Card title="Total Payments" value={`$${totalAmount}`} icon={<FaMoneyBillWave className="size-6" />} />
+                <Card title="Best Payments" value={`$${bestPayment}`} icon={<FaMoneyBillWave className="size-6" />} />
+            </div>
+            <div className='bg-base-300 rounded-3xl p-5 md:p-10'>
+                <PaymentHistory bookings={bookings}></PaymentHistory>
+            </div>
+            <div className='bg-base-300 rounded-3xl p-5 md:p-10'>
+                <BookingHistory bookings={bookings}></BookingHistory>
+            </div>
         </div>
     );
 };
