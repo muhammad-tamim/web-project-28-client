3c88f47a62b5be230bf7915fdbb0d0542e2f98a5
muhammad-tamim <contact2tamim@gmail.com>
2026-02-17 23:36:16 +0600

    feat(dashboard/ManageUsers): add table row for manage user component

---
+++ src/components/dashboard/ManageUsers/TableRowForManageUsers.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import useUpdateUser from '../../../hooks/queries/users/useUpdateUser';
+import toast from 'react-hot-toast';
+
+const TableRowForManageUsers = ({ user }) => {
+    const { mutate: updateUser } = useUpdateUser(user?.email);
+    const [selectedValue, setSelectedValue] = useState('');
+
+    const handleActions = (e) => {
+        setSelectedValue(e.target.value);
+        updateUser({ id: user._id, data: { role: e.target.value } }, {
+            onSuccess: () => {
+                toast.success("User Role Updated Successfully")
+            },
+            onError: (err) => toast.error(err.message || 'Update Failed')
+        })
+    }
+
+    return (
+        <tr>
+            <td className="p-3">
+                <div className="w-24 h-16 overflow-hidden rounded-lg">
+                    <img
+                        src={user.photoUrl}
+                        alt={user.name}
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+            </td>
+
+            <td className='p-3'>{user.name}</td>
+            <td className='p-3'>{user.email}</td>
+            <td className='p-3'>{user.role}</td>
+            <td className='p-3'>{user.wantToSellerRequest ? "true" : "false"}</td>
+            <td>
+                <select onChange={handleActions} value={selectedValue} className="select select-primary focus:outline-none bg-base-300">
+                    <option value={''} disabled={true}>Select an action</option>
+                    <option value={'customer'}>Make Customer</option>
+                    <option value={'seller'}>Make Seller</option>
+                </select>
+            </td>
+        </tr>
+    );
+};
+
+export default TableRowForManageUsers;
