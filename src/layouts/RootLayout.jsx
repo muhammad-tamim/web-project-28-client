5ebe086e5342c2b57d15ada7a93ff728e02ff15c
muhammad-tamim <contact2tamim@gmail.com>
2026-03-16 19:30:59 +0600

    feat(utils): add ScrollToTop

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../components/layouts/Navbar';
 import Footer from '../components/layouts/Footer/Footer';
+import ScrollToTop from '../utils/ScrollToTop';
 
 const RootLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <ScrollToTop></ScrollToTop>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
